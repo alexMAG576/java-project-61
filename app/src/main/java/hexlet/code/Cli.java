bcf5e987cd5eeab42b0d598b4d65665c452a4b9d
MAG <ivan_ivanov576@icloud.com>
2023-10-03 14:52:10 +0300

    created a class Cli described the logic of the scanner

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,2 +1,15 @@
-package hexlet.code;public class Cli {
+package hexlet.code;
+
+import java.util.Scanner;
+public class Cli {
+    public static void beginning() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("What is your name? ");
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+
+        scanner.close();
+    }
 }
