df7d9b0c0e56153c2107131aa7e0de5d8d1e10be
MAG <ivan_ivanov576@icloud.com>
2023-10-09 22:34:53 +0300

    fixed linter errors 2

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -1,12 +1,12 @@
 package hexlet.code.games;
 
 public class Even implements Game {
-    int lowRangeValue = 1;
-    int highRangeValue = 20;
     public final String getRules() {
         return "Answer 'yes' if the number is even, otherwise answer 'no'.";
     }
     public final String[] getGameData() {
+        int lowRangeValue = 1;
+        int highRangeValue = 20;
         String[] gameData = new String[2];
         int question = Even.getRandomNumber(lowRangeValue, highRangeValue);
         gameData[0] = Integer.toString(question);
@@ -14,7 +14,7 @@ public class Even implements Game {
         return gameData;
 
     }
-    public static String getIsEven ( int question){
+    public static String getIsEven (int question){
         if (((question % 2) < 1)) {
             return "yes";
         } else {
@@ -22,9 +22,7 @@ public class Even implements Game {
         }
     }
     public static int getRandomNumber(int lowRangeValue, int highRangeValue) {
-
         double questionDouble = (Math.random() * highRangeValue) + lowRangeValue;
-
         return (int) questionDouble;
     }
 }
